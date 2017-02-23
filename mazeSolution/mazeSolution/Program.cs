2ea5ea19b4d3dd1c19aeefe071971947075fec9a
Kasper Osterbye(win) <koe@cphbusiness.dk>
2017-02-23 14:48:56 +0100

    Commit before class 2017-02-23

---
+++ mazeSolution/mazeSolution/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mazeSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TimingQuestions.TekstTilTal();
+            //TimingQuestions.InstantiateArray();
+            char[,] maze = null;
+            ReadMaze(ref maze, "../../maze.txt");
+            PrintMaze(maze);
+            Console.ReadLine();
+        }
+
+        private static void ReadMaze(ref char[,] maze, string fileLocation)
+        {
+            FileStream fs = new FileStream(fileLocation, FileMode.Open, FileAccess.Read);
+
+            using (StreamReader streamReader = new StreamReader(fs, Encoding.UTF8))
+            {
+                string line = String.Empty;
+                if ((line = streamReader.ReadLine()) != null)
+                {
+                    string[] sizes = line.Split('x');
+                    int width, height;
+                    if (sizes.Count() == 2 && int.TryParse(sizes[0], out width) && int.TryParse(sizes[1], out height))
+                    {
+                        maze = new char[width, height];
+                        int index = 0;
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            for (int i = 0; i < line.Count(); i++)
+                            {
+                                maze[i, index] = (char)line[i];
+                            }
+                            index++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void PrintMaze(char[,] maze)
+        {
+            Console.WriteLine("Maze: " + maze.GetLength(0) + " x " + maze.GetLength(1));
+            for (int j = 0; j < maze.GetLength(0); j++)
+            {
+                for (int i = 0; i < maze.GetLength(1); i++)
+                {
+                    Console.Write(maze[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ mazeSolution/mazeSolution/TimingQuestions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mazeSolution
+{
+    /*
+     * Tidstagning
+    Som led i løsningen på denne opgave skal der som minimum løses følgende (der skal meget mere til dog):
+
+    tallene i den første linje skal laves om fra tekst til heltal (int)
+    der skal allokeres et to dimentionelt array af tegn i passende størrelse
+    opg 1. Prøv at tage tid på disse to dele (konvertering fra tekst til heltal og oprettelse af array).
+
+    opg 2. Hvordan afhænger array allokeringen af størrelsen på arrayet?
+
+    opg 3. Kommer der nogle bemærkelsesværdige ændringer i tidsforbruget når det bliver meget store arrays (når man begynder at allokere nær din computers interne lager størrelse).
+     */
+    class TimingQuestions
+    {
+        public static double TekstTilTal()
+        {
+            int count = 10000000;
+            string[] numbers = new string[1000];
+            Random rnd = new Random();
+            int month = rnd.Next(1, 13);
+            for (int i = 0; i<1000; i++)
+            {
+                numbers[i] = "" + rnd.Next(100000, 3000000);
+            }
+            Console.WriteLine("Heltalslæsning");
+            int dummy = 77;
+            for (int ii = 0; ii < 10; ii++)
+            {
+                Timer t = new Timer();
+                for (int i = 0; i < count; i++)
+                    int.TryParse(numbers[i%1000], out dummy);
+                double time = t.Check() * 1e9 / count;
+                Console.WriteLine("{0,6:F1} ns", time);
+            }
+            return dummy;
+        }
+
+        public static double InstantiateArray()
+        {
+            int count = 10;
+            int[,] iArr;
+            Console.WriteLine("Instantiate Array");
+            int dummy = 77;
+            long kilo = 2 ^ 10; // kilo, mega, giga , 1000, 1000.000, 1000.000.000
+            for (int ii = 0; ii < 10; ii++)
+            {
+                long size = (long)(Math.Pow(2, ii+kilo));
+                Timer t = new Timer();
+                for (int i = 0; i < count; i++)
+                    iArr = new int[size, size];
+                double time = t.Check() * 1e9 / count;
+                Console.WriteLine("{0,6:F1} ns for " + size, time);
+            }
+            return dummy;
+        }
+    }
+
+    public class Timer
+    {
+        private readonly System.Diagnostics.Stopwatch stopwatch
+          = new System.Diagnostics.Stopwatch();
+        public Timer() { Play(); }
+        public double Check() { return stopwatch.ElapsedMilliseconds / 1000.0; }
+        public void Pause() { stopwatch.Stop(); }
+        public void Play() { stopwatch.Start(); }
+    }
+}
